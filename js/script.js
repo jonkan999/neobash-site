85922e559fdc02d1909d4020ff68c6a7467620f9
Jonkan <joel.engstrom17@gmail.com>
2022-10-21 16:36:46 +0200

    made submission page responsive

--- js/script.js
+++ js/script.js
@@ -11,18 +11,6 @@ const currentYear = new Date().getFullYear();
 /* yearEl.textContent = currentYear; */
 
 ///////////////////////////////////////////////////////////
-// Make mobile navigation work
-
-const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(".header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-  /*   var element = document.getElementsByClassName("hero-brand-box");
-  element[0].classList.toggle("hero-brand-box--closed"); */
-});
-
-///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
