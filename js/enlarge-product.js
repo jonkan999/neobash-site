813a186aaebd479c1bf5fa5120e51b16b84f1eeb
Jonkan <joel.engstrom17@gmail.com>
2022-10-28 10:59:23 +0200

    added auxilliary html inclusion to enlargement of products

--- js/enlarge-product.js
+++ js/enlarge-product.js
@@ -1,39 +1,41 @@
 ///////////////////////////////////////////////////////////
 // enlarge clicked product card and blur background
 
-const enlargeEl = document.querySelector(".section-product-grid");
-const appendedHtml = document.querySelector(".appended-html");
+/* const venueSkyddsrummet = $("#venueSkyddsrummet").load(
+  "product-cards/venue-skyddsrummet-clicked.html"
+); */
 
-document.addEventListener("click", function (event) {
+const enlargeEl = document.querySelector(".section-product-grid");
+
+document.addEventListener("click", async function (event) {
   const clickedElement = event.target.closest(".product-card");
   const shadowEl = document.querySelector(".full-page-shadow");
-  // If user clicks inside the element, toggle blur-filter
-  console.log(shadowEl.classList[1]);
+
+  console.log(clickedElement);
   if (event.target.classList[0] === "vendor-link") {
     return;
   }
-  if (clickedElement) {
+  // If user clicks inside the element, enlarge and shadow
+  if (clickedElement && shadowEl.classList[1] != "active-shadow") {
+    console.log("fall1");
     shadowEl.classList.toggle("active-shadow");
-    clickedElement.classList.toggle("product-card-large");
-    //Add HTML if it doesnt exist
-    if (document.querySelector(".appended-html") === null) {
-      clickedElement.children[1].innerHTML +=
-        '<div class="appended-html"><a target="_blank" href="https://skyddsrummet.se/" class="vendor-link"> <span>Go to vendor site </span> <ion-icon name="chevron-forward-outline"></ion-icon> </a> <button class="btn add-button">add</button></div>';
-    } else {
-      //otherwise remove it on click
-      const appendedHtml = document.querySelector(".appended-html");
-      appendedHtml.parentNode.removeChild(appendedHtml);
-    }
-  } else if (shadowEl.classList[1] === "active-shadow") {
-    // If user clicks clicks anywhere else and element is blur-filter, toggle out
+    const src =
+      "product-cards/" + clickedElement.getAttribute("name") + "-clicked.html";
+    const resp = await fetch(src);
+    const venueSkyddsrummet = await resp.text();
+    clickedElement.parentElement.innerHTML += venueSkyddsrummet;
+  } else if (clickedElement && shadowEl.classList[1] === "active-shadow") {
+    // If user clicks inside the element, and it is enlarged, minimize and remove shadow
     shadowEl.classList.toggle("active-shadow");
-    document
-      .querySelector(".product-card-large")
-      .classList.toggle("product-card-large");
-  }
-  //and remove added html
-  const appendedHtml = document.querySelector(".appended-html");
-  if (appendedHtml != null && shadowEl.classList[1] != "active-shadow") {
-    appendedHtml.parentNode.removeChild(appendedHtml);
+    const cardLarge = document.querySelector(".product-card-large");
+    cardLarge.parentNode.removeChild(cardLarge);
+  } else if (
+    clickedElement === null &&
+    shadowEl.classList[1] === "active-shadow"
+  ) {
+    // If user clicks outside, and it is enlarge, minimize and remove shadow
+    shadowEl.classList.toggle("active-shadow");
+    const cardLarge = document.querySelector(".product-card-large");
+    cardLarge.parentNode.removeChild(cardLarge);
   }
 });
