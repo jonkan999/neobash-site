6ccb2c6be19da1509f8f1e3c0aaf09cfbf9c7ea8
Joel Engström <joel.engstrom@if.se>
2022-12-29 12:05:43 +0100

    added light and sound icon

--- js/scrollLowerHeader.js
+++ js/scrollLowerHeader.js
@@ -1,12 +1,12 @@
 export function scrollLowerHeader(scrollBtn) {
 	if (scrollBtn.classList[1] === "lower-header-right") {
 		categoryList.scrollBy({
-			left: 90,
+			left: 0.5 * window.innerWidth,
 			behavior: "smooth",
 		});
 	} else {
 		categoryList.scrollBy({
-			left: -90,
+			left: -0.5 * window.innerWidth,
 			behavior: "smooth",
 		});
 	}
--- js/updateItemCounter.js
+++ js/updateItemCounter.js
@@ -1,11 +1,13 @@
 export function updateItemCounter() {
 	const basketArray = JSON.parse(sessionStorage.getItem("bash-basket"));
 
-	if (
-		itemCounter.classList[1] != "item-counter-active" &&
-		basketArray.length > 0
-	) {
-		itemCounter.classList.add("item-counter-active");
+	if (basketArray) {
+		if (
+			itemCounter.classList[1] != "item-counter-active" &&
+			basketArray.length > 0
+		) {
+			itemCounter.classList.add("item-counter-active");
+		}
+		itemCounter.innerHTML = basketArray.length;
 	}
-	itemCounter.innerHTML = basketArray.length;
 }
