1b0b174a02c893ef8821bd543298e6c5e8069241
Joel Engström <joel.engstrom@if.se>
2023-01-28 12:11:21 +0100

    Added new script for generation of /se site

---
+++ se/generateIndexSE.js
@@ -0,0 +1,119 @@
+const fs = require("fs");
+
+// The object containing the replacement text
+const seText = {
+	heroHeader: "tänk på ditt drömevent. <br> vi får det att ske.",
+	navCategories: "Kategorier",
+	navHowItWorks: "Hur det funkar",
+	navAbout: "Om oss",
+	navBlog: "Blogg",
+	navContact: "kontakta oss",
+	heroContact: "våra produkter",
+	heroEventbuilder: "bygg ditt event",
+	categoriesHeader: "This is the neobash event builder",
+	categoriesHeaderDescription:
+		"Här har vi samlat Stockholms bästa event-leverantörer. Välj en kategori och börja utforska deras lösningar.",
+	categoriesVenue: "lokaler",
+	categoriesWedding: "bröllop",
+	categoriesDrinks: "dryckeslösningar",
+	categoriesEntertainment: "underhållning",
+	categoriesDecor: "dekor",
+	howHeader:
+		"Let us take care of the planning and you just relax and enjoy your party",
+	howHeadersub1: "Beskriv ditt event",
+	howHeadersubContent1:
+		"Fundera igenom hur du vill utforma ditt evenemang. Överväg ditt val av plats,\
+      atmosfär, mat, dryck, underhållning.",
+	howHeadersub2:
+		"Gå igenom våra leverantörers erbjudanden, eller låt oss göra det åt dig!",
+	howHeadersubContent2:
+		"Utforska våra hundratals handutvalda partnerleverantörer och\
+        produkter, för att hitta det som bäst passar det du har föreställt dig. Eller,\
+        eller gå igenom vår automatiserade eventbyggare och matcha\
+        leverantörer och produkter till ditt event.",
+	howHeadersub3: "Skicka in dina val",
+	howHeadersubContent3:
+		"Vårt team av erfarna eventplanerare kommer att få det och\
+        igång med koordineringen.",
+	howHeadersub4: "Slappna av och <strong>njut av</strong> din fest",
+	howHeadersubContent4:
+		"Vi återkommer snarast till dig via mail eller telefon,\
+        som du föredrar. Vi går igenom eventet och tar hand om allt,\
+        medan du kan luta dig tillbaka och slappna av.",
+	howContact: "kontakta oss",
+	contactHeader: "Hör av dig och planera ditt event med oss!",
+	contactParagraph:
+		"Fyll i vårt formulär och någon av våra fantastiska eventhandläggare\
+      kommer återkomma till dig och berättar mer om vad vi kan erbjuda,\
+      samt guidar dig genom planeringsprocessen.",
+	contactNameInput: `
+    <label for="full-name">För och Efternamn</label>
+    <input
+      id="full-name"
+      type="text"
+      placeholder="John Smith"
+      name="full-name"
+      required
+    />`,
+	contactMailInput: `
+    <label for="email">Email</label>
+    <input
+      id="email"
+      type="email"
+      placeholder="jag@example.com"
+      name="email"
+      required
+    />`,
+	contactInquiryInput: `
+    <label for="inquiry">Vad kan vi hjälpa dig med?</label>
+    <textarea
+      id="inquiry"
+      type="inquiry"
+      name="inquiry"
+      placeholder="Jag vill bara veta mer om era grymma tjänster!"
+    ></textarea>
+    <span class="call-us-text" id="contactCall"
+    >Eller ring oss bara på: <br /><a
+      class="call-us-number"
+      href="tel:0765687878"
+      >0765687878</a
+    ></span
+  >
+    `,
+};
+
+// Read the contents of the HTML file C:\Users\ENGJOE\neobash-site\index.html
+fs.readFile("C:/Users/ENGJOE/neobash-site/index.html", "utf8", (err, data) => {
+	if (err) {
+		console.log(err);
+		return;
+	}
+
+	const cheerio = require("cheerio");
+	const $ = cheerio.load(data);
+
+	// Iterate through the keys of the seText object
+	for (const key in seText) {
+		// Replace the inner HTML of the element with the specified ID
+		console.log(`#${key}`);
+		$(`#${key}`).html(seText[key]);
+		/*     data = data.replace(
+			`id="${key}"`,
+			`id="${key}" innerText="${seText[key]}"`
+		);
+		console.log(seText[key]); */
+	}
+
+	// Write the modified HTML to a new file
+	fs.writeFile(
+		"C:/Users/ENGJOE/neobash-site/se/index.html",
+		$.html(),
+		"utf8",
+		(err) => {
+			if (err) {
+				console.log(err);
+				return;
+			}
+		}
+	);
+});
